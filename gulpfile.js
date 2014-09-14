f5cc17e54550c7d3d6d53ae6b6e34a3f9ee1ca2c
Brent Jackson <jacksonblack@gmail.com>
2014-09-13 21:56:07 -0400

    Start on themes

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,18 @@ var vars = require('rework-vars');
 var colors = require('rework-plugin-colors');
 var calc = require('rework-calc');
 
-gulp.task('default', []);
+
+gulp.task('default', ['rework']);
+
+gulp.task('dev', ['watch', 'serve']);
+
+gulp.task('watch', ['rework', 'include'], function() {
+  gulp.watch(['./**/*.html', './src/**/*.css', '!./_site/**/*'], ['rework', 'include', 'reload']);
+});
+
 
 gulp.task('rework', function() {
+  console.log('rework');
   gulp.src('./src/basscss.css')
     .pipe(rework( rnpm(), media(), vars(), colors(), calc ))
     .pipe(autoprefixer())
@@ -22,6 +31,19 @@ gulp.task('rework', function() {
     .pipe(mincss())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('.'));
+});
+
+// Custom file includes
+var include = require('./gulp/include');
+gulp.task('include', function() {
+  gulp.src('./docs/templates/**/*.html')
+    .pipe(include())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('reload', function() {
+  console.log('reload');
+  browsersync.reload();
 });
 
 gulp.task('serve', function() {
