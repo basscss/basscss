967f268429e86204be07cdb6bcc04f42cc8e0aa0
Brent Jackson <jacksonblack@gmail.com>
2014-09-20 17:13:19 -0400

    Basic sassify task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,7 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var mincss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
-var browsersync = require('browser-sync');
 var gzip = require('gulp-gzip');
-var stylestats = require('gulp-stylestats');
 
 var rework = require('gulp-rework');
 var rnpm = require('rework-npm');
@@ -14,15 +12,56 @@ var vars = require('rework-vars');
 var colors = require('rework-plugin-colors');
 var calc = require('rework-calc');
 
-// Custom gulp plugins
-var include = require('./gulp/include');
-var example = require('./gulp/include-example');
-var nav = require('./gulp/is-active');
-var pygmentize = require('./gulp/pygmentize');
+// Site and documentation
+var browsersync = require('browser-sync');
+var stylestats = require('gulp-stylestats');
 
 
-gulp.task('default', ['rework', 'site-rework']);
+gulp.task('default', ['rework']);
 
+gulp.task('rework', function() {
+  gulp.src('./src/*.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc ))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('.'))
+    .pipe(mincss())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('.'))
+    .pipe(gzip())
+    .pipe(gulp.dest('.'));
+});
+
+// Create Sass partials
+gulp.task('sassify', function() {
+  gulp.src('basscss-base/index.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc )).pipe(autoprefixer())
+    .pipe(rename('_base.scss')).pipe(gulp.dest('scss'));
+  gulp.src('basscss-utilities/index.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc )).pipe(autoprefixer())
+    .pipe(rename('_utilities.scss')).pipe(gulp.dest('scss'));
+  gulp.src('basscss-positions/index.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc )).pipe(autoprefixer())
+    .pipe(rename('_positions.scss')).pipe(gulp.dest('scss'));
+  gulp.src('basscss-grid/index.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc )).pipe(autoprefixer())
+    .pipe(rename('_grid.scss')).pipe(gulp.dest('scss'));
+  gulp.src('basscss-table-object/index.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc )).pipe(autoprefixer())
+    .pipe(rename('_table-object.scss')).pipe(gulp.dest('scss'));
+  gulp.src('basscss-color-basic/index.css')
+    .pipe(rework( rnpm(), media(), vars(), colors(), calc )).pipe(autoprefixer())
+    .pipe(rename('_color-basic.scss')).pipe(gulp.dest('scss'));
+});
+
+
+// Stylestats
+gulp.task('stats', function() {
+  gulp.src('./basscss.css')
+    .pipe(stylestats());
+});
+
+
+// Site development
 gulp.task('dev', ['watch', 'serve']);
 
 gulp.task('watch', ['rework', 'site-rework', 'render'], function() {
@@ -40,26 +79,7 @@ gulp.task('serve', function() {
   browsersync({ server: { baseDir: './' }, open: false, ghostMode: false });
 });
 
-
-gulp.task('rework', function() {
-  gulp.src('./src/*.css')
-    .pipe(rework( rnpm(), media(), vars(), colors(), calc ))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('.'))
-    .pipe(mincss())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('.'))
-    .pipe(gzip())
-    .pipe(gulp.dest('.'));
-});
-
-// Stylestats
-gulp.task('stats', function() {
-  gulp.src('./basscss.css')
-    .pipe(stylestats());
-});
-
-// Docs
+// Site stylesheet
 gulp.task('site-rework', function() {
   gulp.src('./docs/css/src/index.css')
     .pipe(rework( rnpm(), media(), vars(), colors(), calc ))
@@ -68,6 +88,12 @@ gulp.task('site-rework', function() {
     .pipe(rename('base.min.css'))
     .pipe(gulp.dest('./docs/css'));
 });
+
+// Build site
+var include = require('./gulp/include');
+var example = require('./gulp/include-example');
+var nav = require('./gulp/is-active');
+var pygmentize = require('./gulp/pygmentize');
 
 gulp.task('render', function() {
   gulp.src('./docs/templates/**/*.html')
