1333f790b253be15dc61ca72efcf1f3d5038d5a9
Brent Jackson <jacksonblack@gmail.com>
2014-09-25 10:51:15 -0400

    Rework docs

--- docs/examples/gulp-recipe.js
+++ docs/examples/gulp-recipe.js
@@ -1,24 +1,14 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var mincss = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var gzip = require('gulp-gzip');
 
-var rework = require('gulp-rework');
-var reworkNPM = require('rework-npm');
-var reworkMedia = require('rework-custom-media');
-var reworkVars = require('rework-vars');
-var reworkColors = require('rework-plugin-colors');
-var reworkCalc = require('rework-calc');
+var basswork = require('gulp-basswork');
 
-gulp.task('rework', function() {
+gulp.task('basswork', function() {
   gulp.src('./src/*.css')
-    .pipe(rework( reworkNPM(), reworkMedia(), reworkVars(), reworkColors(), reworkCalc ))
-    .pipe(autoprefixer())
+    .pipe(basswork())
     .pipe(gulp.dest('./css'))
     .pipe(mincss())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./css'))
-    .pipe(gzip())
-    .pipe(gulp.dest('./css'));
 });
