fbbbd522ccbddc64483d654987ddf569705c84c6
Brent Jackson <jacksonblack@gmail.com>
2014-12-14 22:56:51 -0500

    Modules reference

--- docs/src/modules.js
+++ docs/src/modules.js
@@ -9,9 +9,11 @@ module.exports = function() {
   var dependencies = Object.keys(require('../../package.json').dependencies);
   dependencies.forEach(function(d) {
     var version = require(d + '/package.json').version;
+    var description = require(d + '/package.json').description;
     modules.push({
       name: d,
       version: version,
+      description: description,
       permalink: npmUrl + d
     });
   });
