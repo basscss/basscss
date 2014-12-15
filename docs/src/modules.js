80ae412fe1d73246482abddd6a49a738044a8e73
Brent Jackson <jacksonblack@gmail.com>
2014-12-14 23:05:58 -0500

    Add module descriptions

--- docs/src/modules.js
+++ docs/src/modules.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 module.exports = function() {
 
   var modules = [];
-  var npmUrl = 'http://npmjs.com/package/';
+  var npmUrl = '//www.npmjs.com/package/';
 
   var dependencies = Object.keys(require('../../package.json').dependencies);
   dependencies.forEach(function(d) {
