b7d5a256bac72c830bc829015e4dfacb46c656e6
Brent Jackson <jacksonblack@gmail.com>
2014-12-24 14:06:00 -0500

    Use modules from data model

--- docs/src/modules.js
+++ docs/src/modules.js
@@ -1,23 +1,42 @@
 
 var fs = require('fs');
+var path = require('path');
+var marked = require('marked');
+var markedExample = require('marked-example');
+var camelcase = require('camel-case');
 
 module.exports = function() {
 
-  var result = [];
+  var result = {};
   var npmUrl = '//www.npmjs.com/package/';
   var sources = require('../../package.json').css;
 
+  var exampleOptions = {
+    classes: {
+      container: 'mb2 bg-darken-1 rounded',
+      rendered: 'p2',
+      code: 'm0 p2 bg-darken-1 rounded-bottom'
+    }
+  };
+  var renderer = new marked.Renderer();
+  renderer.code = markedExample(exampleOptions);
+
   var modules = sources.modules;
   modules = modules.concat(sources.optionalModules);
+
   modules.forEach(function(module) {
     var pkg = require(module + '/package.json');
-    result.push({
+    var md = fs.readFileSync('./node_modules/' + module + '/README.md', 'utf8');
+    var content = marked(md, { renderer: renderer });
+
+    result[camelcase(module)] = {
       name: module,
       version: pkg.version,
       description: pkg.description,
+      content: content,
       npmLink: npmUrl + module,
       githubLink: pkg.homepage
-    });
+    };
   });
 
   return result;
--- tasks/module-docs.js
+++ tasks/module-docs.js
@@ -7,6 +7,7 @@ var header = require('gulp-header');
 var footer = require('gulp-footer');
 var marked = require('gulp-marked');
 var markedExample = require('marked-example');
+var camelcase = require('camel-case');
 
 module.exports = function() {
 
@@ -19,9 +20,7 @@ module.exports = function() {
   };
 
   var markedOptions = {
-    renderer: {
-      code: markedExample(exampleOptions)
-    }
+    renderer: { code: markedExample(exampleOptions) }
   };
 
   var sources = require('../package.json').css;
@@ -32,22 +31,18 @@ module.exports = function() {
   modules.forEach(function(module) {
     var filename = './node_modules/' + module + '/README.md';
     var data = require('../node_modules/' + module + '/package.json');
-    //var title = require('../node_modules/' + module + '/package.json').name;
-    var top = '{% extends "layouts/docs.html" %}\n' + 
+    var html = '{% extends "layouts/docs.html" %}\n' + 
       '{% set page = {} %}\n' +
       '{% set page.title = "' + module + '" %}\n' +
       '{% set isdocs = true %}\n' +
       '{% set ismodule = true %}\n' +
-      '{% set module = {} %}\n' +
+      '{% set module = modules.' + camelcase(module) + ' %}\n' +
       '{% set module.version = "' + data.version + '" %}\n' +
-      '{% block content %}\n\n';
-    var bottom = '\n\n{% endblock %}\n';
-    gulp.src(filename)
-      .pipe(marked(markedOptions))
-      .pipe(header(top))
-      .pipe(footer(bottom))
-      .pipe(rename('index.html'))
-      .pipe(gulp.dest('./docs/src/templates/docs/modules/' + module));
+      '{% block content %}\n\n' +
+      '{{ module.content | safe }}\n' +
+      '\n\n{% endblock %}\n';
+
+    fs.writeFileSync(path.join(__dirname, '../docs/src/templates/docs/modules/' + module + '/index.html'), html);
   });
 
 };
