85a634a10285d1b36a945ab5d199131e2d6f11ab
Brent Jackson <jxnblk@github.com>
2014-12-30 14:15:11 -0500

    Remove old css-statistics and fix aws config issue

--- tasks/s3.js
+++ tasks/s3.js
@@ -1,10 +1,10 @@
 
 var gulp = require('gulp');
 var s3 = require('gulp-s3');
-var config = require('../aws.json');
 
 module.exports = function() {
   var version = require('../package.json').version;
+  var config = require('../aws.json');
   gulp.src('./css/*.gz')
     .pipe(s3(config, {
       uploadPath: 'basscss/' + version + '/'
