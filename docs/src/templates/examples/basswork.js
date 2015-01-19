ec48e0a2b102ce04f3030a7fd26b1d9ab96902ad
Brent Jackson <jacksonblack@gmail.com>
2015-01-19 16:56:50 -0500

    Remove examples folder and macro in favor of markdown docs

--- docs/src/templates/examples/basswork.js
+++
@@ -1,6 +0,0 @@
-var basswork = require('basswork');
-var fs = require('fs');
-
-var src = fs.readFileSync('./src/base.css', 'utf8');
-var css = basswork(src);
-fs.writeFileSync('./css/base.css', css);
