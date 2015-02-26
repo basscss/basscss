6a4606c67e8061a10e6f0ec9ed7644f6a9371879
Brent Jackson <jacksonblack@gmail.com>
2015-02-25 23:14:01 -0500

    Add stats and raw css source to module pages

--- docs/src/helpers.js
+++ docs/src/helpers.js
@@ -1,8 +1,18 @@
 
+var Humanize = require('humanize-plus');
+var hljs = require('highlight.js');
+
 module.exports = {
   removePrefix: function(string) {
     if (!string) return false;
     var result = string.replace(/^Basscss|^basscss-/, '');
     return result;
-  }
+  },
+  fileSize: function(n) {
+    return Humanize.fileSize(n);
+  },
+  highlightCss: function(string) {
+    //return hljs.highlightAuto(string).value;
+    return hljs.highlight('css', string).value;
+  },
 };
