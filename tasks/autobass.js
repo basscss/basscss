c9e1ce962a352c17656ffdd11c77b12f75c5e01f
Brent Jackson <jacksonblack@gmail.com>
2015-02-25 23:56:59 -0500

    Section headers

--- tasks/autobass.js
+++ tasks/autobass.js
@@ -47,6 +47,7 @@ data.partials['page-header'] = fs.readFileSync('./docs/src/partials/page-header.
 data.partials['showcase-widget'] = fs.readFileSync('./docs/src/partials/showcase-widget.html', 'utf8');
 data.partials.pagination = fs.readFileSync('./docs/src/partials/pagination.html', 'utf8');
 data.partials['color-combo-card'] = fs.readFileSync('./docs/src/partials/color-combo-card.html', 'utf8');
+data.partials['section-header'] = fs.readFileSync('./docs/src/partials/section-header.html', 'utf8');
 
 data.helpers = require('../docs/src/helpers');
 
