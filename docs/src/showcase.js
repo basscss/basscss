047e606317b400c7d688ce0dbf32765f4d8db88f
Brent Jackson <jacksonblack@gmail.com>
2015-03-03 09:45:59 -0500

    Update showcase

--- docs/src/showcase.js
+++ docs/src/showcase.js
@@ -1,16 +1,22 @@
 
 module.exports = {
+  nodeconf: {
+    title: 'Nodeconf',
+    url: 'http://nodeconf.com/',
+    description: 'The longest running community driven conference for the Node community.',
+    image: 'screenshot-nodeconf-2015-03-03-09-23-07-640.jpg '
+  },
+  assembly: {
+    title: 'Assembly',
+    url: 'http://assembly.com/',
+    description: 'Turn ideas into great products with people around the world.',
+    image: 'screenshot-assembly-discover-2015-03-03-09-24-09-640.jpg'
+  },
   monodraw: {
     title: 'Monodraw for Mac',
     url: 'http://monodraw.helftone.com/',
     description: 'Powerful ASCII art editor designed for the Mac.',
     image: 'monodraw-640.jpg'
-  },
-  assembly_blog: {
-    title: 'Assembly Blog',
-    url: 'http://blog.assembly.com/',
-    description: '',
-    image: 'assembly_blog-640.jpg'
   },
   singlesclub: {
     title: 'Singles Club',
@@ -18,12 +24,24 @@ module.exports = {
     description: 'Part record club. Part music journal.',
     image: 'singlesclub-640.jpg'
   },
+  lachlanjc: {
+    title: 'Lachlan Campbell',
+    url: 'http://lachlanjc.me',
+    description: 'Personal site of Lachlan Campbell',
+    image: 'screenshot-lachlanjc--2015-02-24-12-07-33-640.jpg'
+  },
   pixyll: {
     title: 'Pixyll',
     url: 'http://pixyll.com/',
     description: 'A simple, beautiful theme for Jekyll that emphasizes content rather than aesthetic fluff.',
     image: 'pixyll-640.jpg'
   },
+  //scienceparkse: {
+  //  title: 'Science Park',
+  //  url: 'http://www.sciencpark.se/',
+  //  description: '', // No meta tag description
+  //  image: 'screenshot-sciencepark--2015-02-24-12-01-08-640.jpg'
+  //},
   nurturingability: {
     title: 'Nurturing Ability',
     url: 'http://www.nurturingability.com.au/',
@@ -34,7 +52,7 @@ module.exports = {
     title: 'Linknest',
     url: 'https://linknest.cc/',
     description: 'Bookmarks made simple',
-    image: 'linknest-640.jpg'
+    image: 'screenshot-linknest.cc-2015-03-03-09-26-47-640.jpg'
   },
   cssstats: {
     title: 'CSS Stats',
