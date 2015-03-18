516e57d9a50f718adb1d87a8ba786e0b32b8bc63
Brent Jackson <jxnblk@github.com>
2015-03-18 11:45:09 -0400

    Adjust test loop

--- test/index.js
+++ test/index.js
@@ -5,7 +5,6 @@ var mocha = require('mocha');
 var assert = require('assert');
 var cssstats = require('cssstats');
 var Cleancss = require('clean-css');
-var diff = require('diff');
 
 var rework = require('rework');
 var reworkCalc = require('rework-calc');
@@ -16,16 +15,22 @@ var reworkColors = require('rework-plugin-colors');
 var autoprefixer = require('autoprefixer');
 
 var basswork = require('basswork');
-
-var postcss = require('postcss');
-
 var cssnext = require('cssnext');
 var myth = require('myth');
 var suitcss = require('suitcss-preprocessor');
 
+var postcss = require('postcss');
+
 var src = fs.readFileSync(path.join(__dirname, '../src/basscss.css'), 'utf8');
+var processors = [
+  'basswork',
+  'cssnext',
+  'myth',
+  'suitcss',
+];
 
 var results = {
+  release: fs.readFileSync(path.join(__dirname, '../css/basscss.css'), 'utf8'),
   basswork: basswork(src),
   cssnext: cssnext(src),
   myth: myth(src),
@@ -40,33 +45,21 @@ fs.writeFileSync(path.join(__dirname, './results/myth.css'), results.myth);
 fs.writeFileSync(path.join(__dirname, './results/suitcss.css'), results.suitcss);
 
 var minified = {
+  release: new Cleancss().minify(results.release).styles,
   basswork: new Cleancss().minify(results.basswork).styles,
   cssnext: new Cleancss().minify(results.cssnext).styles,
   myth: new Cleancss().minify(results.myth).styles,
   suitcss: new Cleancss().minify(results.suitcss).styles,
 };
 
-
-var diffs = {
-  cssnext: diff.diffCss(minified.basswork, minified.cssnext),
-};
-
-diffs.cssnext.forEach(function(part) {
-  var color = part.added ? 'green' :
-      part.removed ? 'red' : 'grey';
-  //console.log('diff added/removed', part.added, part.removed);
-  if (part.added || part.removed) {
-    console.log(part.added ? 'Added: ' : 'Removed: ' + part.value );
-  }
-  //process.stderr.write(part.value[color]);
-});
-
 var stats = {
+  release: cssstats(minified.release),
   basswork: cssstats(minified.basswork),
   cssnext: cssstats(minified.cssnext),
   myth: cssstats(minified.myth),
   suitcss: cssstats(minified.suitcss),
 };
+
 
 //var CleanCSS = require('clean-css');
 //var source = 'a{font-weight:bold;}';
@@ -81,48 +74,21 @@ describe('basscss-processors', function() {
     assert.equal(typeof results.suitcss, 'string');
   });
 
+  function testStats(processor) {
+    it('Should produce same stats selectors results as ' + processor, function() {
+      assert.equal(stats.release.aggregates.selectors, stats[processor].aggregates.selectors);
+    });
+    it('Should produce same stats declarations results as ' + processor, function() {
+      assert.equal(stats.release.aggregates.declarations, stats[processor].aggregates.declarations);
+    });
+    it('Should produce same stats properties results as ' + processor, function() {
+      assert.equal(stats.release.aggregates.properties.length, stats[processor].aggregates.properties.length);
+    });
+  };
 
-  // cssnext
-  it('Should produce same stats selectors results as cssnext', function() {
-    assert.equal(stats.basswork.aggregates.selectors, stats.cssnext.aggregates.selectors);
+  processors.forEach(function(processor) {
+    testStats(processor);
   });
-
-  it('Should produce same stats declarations results as cssnext', function() {
-    assert.equal(stats.basswork.aggregates.declarations, stats.cssnext.aggregates.declarations);
-  });
-
-  it('Should produce same stats properties results as cssnext', function() {
-    assert.equal(stats.basswork.aggregates.properties.length, stats.cssnext.aggregates.properties.length);
-  });
-
-  /*
-    // myth
-    it('Should produce same stats selectors results as myth', function() {
-      assert.equal(stats.basswork.aggregates.selectors, stats.myth.aggregates.selectors);
-    });
-
-    it('Should produce same stats declarations results as myth', function() {
-      assert.equal(stats.basswork.aggregates.declarations, stats.myth.aggregates.declarations);
-    });
-
-    it('Should produce same stats properties results as myth', function() {
-      assert.equal(stats.basswork.aggregates.properties.length, stats.myth.aggregates.properties.length);
-    });
-
-
-    // suitcss
-    it('Should produce same stats selectors results as suitcss', function() {
-      assert.equal(stats.basswork.aggregates.selectors, stats.suitcss.aggregates.selectors);
-    });
-
-    it('Should produce same stats declarations results as suitcss', function() {
-      assert.equal(stats.basswork.aggregates.declarations, stats.suitcss.aggregates.declarations);
-    });
-
-    it('Should produce same stats properties results as suitcss', function() {
-      assert.equal(stats.basswork.aggregates.properties.length, stats.suitcss.aggregates.properties.length);
-    });
-  */
 
 
 });
