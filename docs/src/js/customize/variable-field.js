0ca248078bd1b760ce12379f2bcaae4c0fc1528a
Brent Jackson <jacksonblack@gmail.com>
2015-04-05 18:34:58 -0400

    Remove old customizer app js

--- docs/src/js/customize/variable-field.js
+++
@@ -1,9 +0,0 @@
-
-var fs = require('fs');
-
-module.exports = function() {
-  return {
-    scope: true,
-    template: fs.readFileSync('./docs/src/js/customize/variable-field.html.ng', 'utf8')
-  }
-};
