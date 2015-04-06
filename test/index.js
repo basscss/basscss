ad1231e28e5fece87e0f8041f5b5f93b09039c49
Brent Jackson <jacksonblack@gmail.com>
2015-04-05 23:19:10 -0400

    Update base buttons

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
 
 var fs = require('fs');
 var path = require('path');
-var mocha = require('mocha');
 var assert = require('assert');
 var cssnext = require('cssnext');
 
 var processors = require('./processors');
 var scss = require('./scss');
-var usage = require('./doiuse');
+//var validate = require('./validate');
+//var usage = require('./doiuse');
 
 var src = fs.readFileSync(path.join(__dirname, '../src/basscss.css'), 'utf8');
 
@@ -17,6 +17,7 @@ describe('basscss', function() {
 
   processors(src);
   scss();
+  //validate(css);
   //usage(css);
 
 });
