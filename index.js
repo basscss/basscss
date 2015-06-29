d795d4d140ecbe535f212a318f0982d0a7432508
jxnblk <jacksonblack@gmail.com>
2015-06-29 09:37:21 -0400

    Add index.js and update base-typography

---
+++ index.js
@@ -0,0 +1,51 @@
+
+module.exports = {
+  modules: [
+    'basscss-base-reset',
+    'basscss-base-forms',
+    'basscss-base-tables',
+    'basscss-base-typography',
+    'basscss-color-base',
+    'basscss-color-forms',
+    'basscss-color-tables',
+    'basscss-btn',
+    'basscss-btn-primary',
+    'basscss-btn-outline',
+    'basscss-type-scale',
+    'basscss-utility-typography',
+    'basscss-utility-layout',
+    'basscss-align',
+    'basscss-white-space',
+    'basscss-positions',
+    'basscss-responsive-states',
+    'basscss-grid',
+    'flex-object',
+    'basscss-borders',
+    'basscss-colors',
+    'basscss-background-colors'
+  ],
+  optional_modules: [
+    'basscss-background-images',
+    'basscss-base-buttons',
+    'basscss-border-colors',
+    'basscss-button-link',
+    'basscss-button-outline',
+    'basscss-button-sizes',
+    'basscss-button-solid',
+    'basscss-button-transparent',
+    'basscss-color-buttons',
+    'basscss-color-forms-dark',
+    'basscss-color-input-range',
+    'basscss-color-progress',
+    'basscss-highlight',
+    'basscss-input-range',
+    'basscss-progress',
+    'basscss-responsive-white-space',
+    'basscss-table-object',
+    'basscss-ui-utility-groups',
+    'basscss-utility-headings'
+  ],
+  variables: [
+    'basscss-defaults'
+  ]
+}
