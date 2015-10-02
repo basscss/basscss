8a3486a805daba44b88e572890b23248cb5d3e44
jxnblk <jacksonblack@gmail.com>
2015-10-02 21:23:05 +0300

    Update specs

--- test/base-tables.spec.js
+++ test/base-tables.spec.js
@@ -3,23 +3,64 @@ import expect from 'expect'
 
 describe('base-tables', () => {
 
-  const getStyle = (el) => ( window.getComputedStyle(el) )
+  document.body.style.width = '768px'
 
-  let table = document.createElement('table')
+  const addElement = (el, parent) => {
+    const $el = document.createElement(el)
+    parent = parent || document.body
+    parent.appendChild($el)
+    $el.computedStyle = window.getComputedStyle($el)
+    return $el
+  }
 
-  beforeEach(() => {
-    document.body.appendChild(table)
+  const table = addElement('table')
+  const th = addElement('th', table)
+  const td = addElement('td', table)
+
+  it('should set table border-collapse', () => {
+    expect(table.computedStyle.borderCollapse).toEqual('separate')
   })
 
-  it('should set table border-collapse')
-  it('should set table border-spacing')
-  it('should set table max-width')
-  it('should set table width')
-  it('should set th text-align')
-  it('should set th font-weight')
-  it('should set th padding')
-  it('should set td padding')
-  it('should set th vertical-align')
-  it('should set td vertical-align')
+  it('should set table border-spacing', () => {
+    expect(table.computedStyle.borderSpacing).toEqual('0px 0px')
+  })
+
+  it('should set table max-width', () => {
+    expect(table.computedStyle.maxWidth).toMatch(/100%|768px/)
+  })
+
+  it('should set table width', () => {
+    expect(table.computedStyle.width).toEqual('768px')
+  })
+
+  it('should set th text-align', () => {
+    expect(th.computedStyle.textAlign).toEqual('left')
+  })
+
+  it('should set th font-weight', () => {
+    expect(th.computedStyle.fontWeight).toMatch(/bold|700/)
+  })
+
+  it('should set th padding', () => {
+    expect(th.computedStyle.paddingTop).toEqual('4px')
+    expect(th.computedStyle.paddingBottom).toEqual('4px')
+    expect(th.computedStyle.paddingLeft).toEqual('16px')
+    expect(th.computedStyle.paddingRight).toEqual('16px')
+  })
+
+  it('should set td padding', () => {
+    expect(td.computedStyle.paddingTop).toEqual('4px')
+    expect(td.computedStyle.paddingBottom).toEqual('4px')
+    expect(td.computedStyle.paddingLeft).toEqual('16px')
+    expect(td.computedStyle.paddingRight).toEqual('16px')
+  })
+
+  it('should set th vertical-align', () => {
+    expect(th.computedStyle.verticalAlign).toEqual('bottom')
+  })
+
+  it('should set td vertical-align', () => {
+    expect(td.computedStyle.verticalAlign).toEqual('top')
+  })
 
 })
--- test/base-typography.spec.js
+++ test/base-typography.spec.js
@@ -1,40 +1,110 @@
 
 import expect from 'expect'
 
-describe('base-typograph', () => {
+describe('base-typography', () => {
 
   const getStyle = (el) => ( window.getComputedStyle(el) )
 
-  let body = document.body
-  let h1 = document.createElement('h1')
+  const addElement = (el) => {
+    const $el = document.createElement(el)
+    body.appendChild($el)
+    return $el
+  }
 
-  beforeEach(() => {
-    document.body.appendChild(h1)
+  const body = document.body
+  const h1 = addElement('h1')
+  const h2 = addElement('h2')
+  const h3 = addElement('h3')
+  const h4 = addElement('h4')
+  const h5 = addElement('h5')
+  const h6 = addElement('h6')
+
+  const p = addElement('p')
+  const ul = addElement('ul')
+  const pre = addElement('pre')
+
+  const fontFamily = /\"Helvetica Neue\"\,Helvetica\,sans-serif|\'Helvetica Neue\'\,\ Helvetica\,\ sans-serif/
+
+  it('should set body font-family', () => {
+    expect(getStyle(body).fontFamily).toMatch(fontFamily)
   })
 
-  it('should set body font-family')
-  it('should set body line-height')
-  it('should set body font-size')
+  it('should set body line-height', () => {
+    expect(getStyle(body).lineHeight).toEqual('24px')
+  })
 
-  it('should set h1 font-family')
-  it('should set h1 font-weight')
-  it('should set h1 line-height')
-  it('should set h1 margins')
+  it('should set body font-size', () => {
+    expect(getStyle(body).fontSize).toEqual('16px')
+  })
 
-  it('should set p margins')
+  it('should set h1 font-family', () => {
+    expect(getStyle(h1).fontFamily).toMatch(fontFamily)
+  })
 
-  it('should set ul margins')
+  it('should set h1 font-weight', () => {
+    expect(getStyle(h1).fontWeight).toMatch(/700|bold/)
+  })
 
-  it('should set pre font-family')
-  it('should set pre font-size')
-  it('should set pre margins')
-  it('should set pre overflow-x')
+  it('should set h1 line-height', () => {
+    expect(getStyle(h1).lineHeight).toEqual('40px')
+  })
 
-  it('should set h1 font-size')
-  it('should set h2 font-size')
-  it('should set h3 font-size')
-  it('should set h4 font-size')
-  it('should set h5 font-size')
-  it('should set h6 font-size')
+  it('should set h1 margins', () => {
+    expect(getStyle(h1).marginTop).toEqual('32px')
+    expect(getStyle(h1).marginBottom).toEqual('16px')
+  })
+
+  it('should set p margins', () => {
+    expect(getStyle(p).marginTop).toMatch(/0px|^$/)
+    expect(getStyle(p).marginBottom).toEqual('16px')
+  })
+
+  it('should set ul margins', () => {
+    expect(getStyle(ul).marginTop).toMatch(/0px|^$/)
+    expect(getStyle(ul).marginBottom).toEqual('16px')
+  })
+
+  it('should set pre font-family', () => {
+    // const preFontFamily = /\"Source\ Code\ Pro\"\,\ Consolas\,\ monospace|\"Source\ Code\ Pro\"\,Consolas\,monospace/
+    const preFontFamily = /(\"|\')Source\ Code\ Pro(\"|\')\,\ ?Consolas\,\ ?monospace/
+    expect(getStyle(pre).fontFamily).toMatch(preFontFamily)
+  })
+
+  it('should set pre font-size', () => {
+    expect(getStyle(pre).fontSize).toEqual('16px')
+  })
+
+  it('should set pre margins', () => {
+    expect(getStyle(pre).marginTop).toMatch(/0px|^$/)
+    expect(getStyle(pre).marginBottom).toEqual('16px')
+  })
+
+  it('should set pre overflow-x', () => {
+    expect(getStyle(pre).overflowX).toEqual('scroll')
+  })
+
+  it('should set h1 font-size', () => {
+    expect(getStyle(h1).fontSize).toEqual('32px')
+  })
+
+  it('should set h2 font-size', () => {
+    expect(getStyle(h2).fontSize).toEqual('24px')
+  })
+
+  it('should set h3 font-size', () => {
+    expect(getStyle(h3).fontSize).toEqual('20px')
+  })
+
+  it('should set h4 font-size', () => {
+    expect(getStyle(h4).fontSize).toEqual('16px')
+  })
+
+  it('should set h5 font-size', () => {
+    expect(getStyle(h5).fontSize).toEqual('14px')
+  })
+
+  it('should set h6 font-size', () => {
+    expect(getStyle(h6).fontSize).toEqual('12px')
+  })
 
 })
