4cdf3e343dbfc0bb4489905b639a3c6b694ac9d9
Ben Brandt <benjamin.j.brandt@gmail.com>
2018-10-10 09:52:00 -0700

    Don't preserve custom properties

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   plugins: [
-    'postcss-import',
-    'postcss-custom-media',
-    'postcss-custom-properties',
-    'postcss-calc',
-    'cssstats',
-    'postcss-discard-comments',
-    'postcss-remove-root',
-    'autoprefixer',
-    'postcss-reporter'
-  ].map(require)
+    require('postcss-import'),
+    require('postcss-custom-media'),
+    require('postcss-custom-properties')({ preserve: false }),
+    require('postcss-calc'),
+    require('cssstats'),
+    require('postcss-discard-comments'),
+    require('postcss-remove-root'),
+    require('autoprefixer'),
+    require('postcss-reporter')
+  ]
 }
