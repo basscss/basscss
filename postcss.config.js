5245268b1a2060cfc979ec8f8af466862e2221fa
Brent Jackson <jxnblk@gmail.com>
2019-05-08 11:58:39 -0400

    Remove autoprefixer and other postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,5 @@ module.exports = {
     require('postcss-custom-properties', {
       preserve: true
     }),
-    require('postcss-calc'),
-    require('postcss-discard-comments'),
-    require('autoprefixer'),
-    require('postcss-reporter')
   ]
 }
